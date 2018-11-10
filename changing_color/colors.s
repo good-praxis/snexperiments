a4af99ef77fc5f0afe308dcc2d3c38bebeeec561
Praxis | Darian <hello@vulpinecat.com>
2018-11-10 22:48:09 +0100

    preliminary (broken) version. Done for the night

--- changing_color/colors.s
+++ changing_color/colors.s
@@ -7,52 +7,43 @@
 
 Main:
         VBL_on
-        stz     $2122
-        lda     #$4F
-        sta     $2122
+        lda #%00011111
+        sta $2122
         stz $2122
-        stz $2122
-        lda     #$0F
-        sta     $2100
-        stz $2122
-        stz $2122
-        stz     $2100
         lda     #$0F
         sta     $2100
         jmp     increment
 
 increment:
-        stz $2122
-        stz $2122
-        lda     $2122
-        cmp     #$FE
-        beq     decrement
+        wai
+        ; ldx     $2122
+        ; lda     $2122 
+        ; asl     a 
+        ; sta     $2122
+        ; txa     
+        ; asl     a
+        ; sta     $2122
+        rol     $2122
+        rol     $2122
 
-        inc     $2122
-        jsr     wait
+        lda     #$0F
+        sta     $2100
         jmp     increment
 
-decrement:
-        jmp     wait
-        lda     $2122
-        cmp     #$00
-        beq     increment
+; decrement:
+;         jmp increment
+;         wai
+;         lda     $2122
+;         cmp     #$00
+;         beq     increment
 
-        dec     $2122
-        jsr     wait
-        jmp     decrement
+;         dec     $2122
+;         stx     $2100
+;         jmp     decrement
 
-wait:
-        lda     $4212
-        cmp     #$40
-        beq     return
-        jmp     wait
-
-return:
-        stz $2122
-        stz $2122
-        rts
-
-end:
+; end:    
+;         wai
+;         stx     $2100
+;         jmp end
 
         
