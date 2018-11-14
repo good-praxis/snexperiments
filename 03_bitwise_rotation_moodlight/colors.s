7cc35bb8ecdf2ce5c65386d3ed94fbc058a2e26d
Praxis | Darian <hello@vulpinecat.com>
2018-11-14 19:08:23 +0100

    adding current status, somehow only first byte works as expected, not the second one

--- 03_bitwise_rotation_moodlight/colors.s
+++ 03_bitwise_rotation_moodlight/colors.s
@@ -7,43 +7,57 @@
 
 Main:
         VBL_on
-        lda #%00011111
-        sta $2122
-        stz $2122
+        stz     $2121
+
+        stz     $2122
+        lda     #%00011111
+        sta     $2122
+        
         lda     #$0F
         sta     $2100
-        jmp     increment
+        
+        jmp     mainloop
 
-increment:
+mainloop:
+        wai             ; waiting for interrupt
+        clc
+        sec             ; making sure we will use the carry flag
+        
+        stz     $2121
+        lda     $213b   ; reading from CG-RAM
+        rol     a       ; rotating with carry
+        tax             ; storing into x for now
+        lda     $213b
+        rol     a
+        tay             ; transfering to y for now
+        
+        txa
+        adc     #$00    ; adding 0, this will add the carry bit if set
+        
+        stz     $2121   ; resetting address
+
+        sta     $2122
+        tya
+        sta     $2122
+
+
+
+loopSetup:     
+        lda     #$1F    ; setting up the loop counter
+        sta     $0000
+loop:
+        lda     #$0F    
+        sta     $2100   ; turning on the screen before doing anything else
+        
+        lda     $0000
+        cmp     #$00    ; if counter is 0
+        beq     mainloop       
+
+        dec     a
+        sta     $0000
+
         wai
-        ; ldx     $2122
-        ; lda     $2122 
-        ; asl     a 
-        ; sta     $2122
-        ; txa     
-        ; asl     a
-        ; sta     $2122
-        rol     $2122
-        rol     $2122
+        jmp     loop
 
-        lda     #$0F
-        sta     $2100
-        jmp     increment
-
-; decrement:
-;         jmp increment
-;         wai
-;         lda     $2122
-;         cmp     #$00
-;         beq     increment
-
-;         dec     $2122
-;         stx     $2100
-;         jmp     decrement
-
-; end:    
-;         wai
-;         stx     $2100
-;         jmp end
 
         
